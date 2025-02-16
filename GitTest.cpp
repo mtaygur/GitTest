06fdbc5dcd175c89df5e4925bf11f816789f0de5
Mehmet Mert Taygur <mtaygur@hotmail.com>
2025-02-17 00:39:20 +0100

    b1c1

--- GitTest.cpp
+++ GitTest.cpp
@@ -7,15 +7,10 @@ using namespace std;
 
 int main()
 {
-	cout << "b1c1" << endl;
-	cout << "b1c2" << endl;
-	cout << "b1c3" << endl;
 
 
 	double ft2c1;
 	double ft2c2;
-	int ft1c1;
-	int ft1c2;
 
 	return 0;
 }
